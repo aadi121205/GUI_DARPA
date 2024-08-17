ae4d90c7e18503a83586591ddeed85bc1c1f0f9e
Aadi <aaditya12120205@gmail.com>
2024-08-17 20:41:38 +0530

    update

--- front/src/Comps/Control.jsx
+++ front/src/Comps/Control.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 import { SocketState } from "../context/socketContext";
 import { TelemState } from "../context/home/telemState";
 import Contexp from "./ControlComps/Contexp";
 import Telemexp from "./TelemComps/Teleexp";
 
 export default function Control() {
-
-    return (
-        <SocketState>
-        <TelemState>
-            <Contexp/>
-        </TelemState>
-      </SocketState>
-    );
-    }
+  return (
+    <SocketState>
+      <TelemState>
+        <Contexp />
+      </TelemState>
+    </SocketState>
+  );
+}
--- front/src/Comps/Nav.jsx
+++ front/src/Comps/Nav.jsx
@@ -1,10 +1,19 @@
 import * as React from "react";
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 
 function NavigationBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Container style={{ padding: "3px", display: "flex", justifyContent: "flex-start", alignItems: "left", margin: "0.5px", paddingLeft: "30px"}}>
+      <Container
+        style={{
+          padding: "3px",
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "left",
+          margin: "0.5px",
+          paddingLeft: "30px",
+        }}
+      >
         <Navbar.Brand href="/">
           <img
             src="src/assets/uas_logo.png" // replace with the path to your image
@@ -14,7 +23,12 @@ function NavigationBar() {
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse bg="dark" variant="dark" expand="lg" id="basic-navbar-nav">
+        <Navbar.Collapse
+          bg="dark"
+          variant="dark"
+          expand="lg"
+          id="basic-navbar-nav"
+        >
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/telem">Telem</Nav.Link>
@@ -30,7 +44,15 @@ function NavigationBar() {
           </Nav>
         </Navbar.Collapse>
       </Container>
-      <Container style={{ padding: "3px", display: "flex", justifyContent: "flex-end", paddingLeft: "0px", overflow: "hidden" }}>
+      <Container
+        style={{
+          padding: "3px",
+          display: "flex",
+          justifyContent: "flex-end",
+          paddingLeft: "0px",
+          overflow: "hidden",
+        }}
+      >
         <Navbar.Brand href="https://triagechallenge.darpa.mil/">
           <img
             src="src/assets/darpa_logo.png" // replace with the path to your image
