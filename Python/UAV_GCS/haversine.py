50461b4bb955de3296da8aaefa63af01b59546aa
Aadi <aaditya12120205@gmail.com>
2025-02-22 01:57:32 +0530

    Clean Slate for Phase 2

--- Python/UAV_GCS/haversine.py
+++
@@ -1,7 +0,0 @@
-from math import radians, cos, sin, asin, sqrt
-
-def haversine(lon1, lat1, lon2, lat2):
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    a = sin((lat2-lat1)/2)**2 + cos(lat1) * cos(lat2) * sin((lon2-lon1)/2)**2
-    c = 2 * asin(sqrt(a))
-    return c * 6371 * 1000
--- front/src/context/home/telemContext.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-const telemContext = createContext();
-console.log(telemContext);
-
-export default telemContext;
