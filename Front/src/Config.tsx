6bfc933222bc3baea93f70c9919578c271e98ae9
Aaditya Bhatia <aaditya.tec@gmail.com>
2025-06-06 20:20:37 +0530

    update

--- Front/src/Config.tsx
+++
@@ -1,5 +0,0 @@
-const protocol = 'https:';
-const host = window.location.hostname;
-const port = '3000'; // or any other port you're using
-export const SOCKET_URL = `${protocol}//${host}:${port}`;
-console.log(SOCKET_URL);
